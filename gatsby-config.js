67f782ed46a7e99b177572337cac56971b15d39e
joeyology <josemari.taleno@gmail.com>
2020-03-25 08:41:33 +0800

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,12 @@ module.exports = {
                 pageContextProperty: `menus`,
                 menus: require('./src/data/menus.json'),
             }
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: 'UA-125354139-4'
+            }
         }
     ]
 };
