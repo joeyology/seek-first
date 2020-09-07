47952d6467c00a13f3db8496e228c5529e28f4c4
joeyology <josemari.taleno@gmail.com>
2020-09-07 14:31:01 +0800

    Twitter Card Update
    Test Twitter Card

--- src/html.js
+++ src/html.js
@@ -6,7 +6,6 @@ export default function HTML(props) {
     <html {...props.htmlAttributes}>
       <head>
         {props.headComponents}
-        <script data-name="BMC-Widget" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="seekfirst" data-description="Support me on Buy me a coffee!" data-message="Thank you for visiting. You can now buy me a coffee!" data-color="#79D6B5" data-position="" data-x_margin="18" data-y_margin="18"></script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
