fcdaf33dcb107e73e021fd316e1e5d644e78f83d
joeyology <josemari.taleno@gmail.com>
2020-09-07 16:06:58 +0800

    Update post.js
    Adjust Date and Author

--- src/templates/post.js
+++ src/templates/post.js
@@ -15,14 +15,13 @@ export default class Post extends React.Component {
                   <header className="post-header inner-small">
                     <h1 className="post-title line-top">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
                     <div className="post-meta">
-                      {_.get(this.props, 'pageContext.site.data.author.name') && 
-                      <span className="published">
-                        ✍ by {_.get(this.props, 'pageContext.site.data.author.name')},
-                      </span>
-                      }
-                      <span> </span>
                       <time className="published"
                         dateTime={moment(_.get(this.props, 'pageContext.frontmatter.date')).strftime('%Y-%m-%d %H:%M')}>{moment(_.get(this.props, 'pageContext.frontmatter.date')).strftime('%B %d, %Y')}</time>
+                      {_.get(this.props, 'pageContext.site.data.author.name') && 
+                      <span className="published">
+                        ✍ by {_.get(this.props, 'pageContext.site.data.author.name')}
+                      </span>
+                      }
                     </div>
                     {_.get(this.props, 'pageContext.frontmatter.subtitle') && 
                     <div className="post-subtitle">
