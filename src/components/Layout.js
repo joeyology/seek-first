8b132440b92d414fb550fa167676b9f121e61c8a
joeyology <josemari.taleno@gmail.com>
2020-09-07 16:15:02 +0800

    Update Layout.js
    Add Favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -21,6 +21,7 @@ export default class Body extends React.Component {
                     <meta name="twitter:site" content="https://www.seekfirst.ltd" />
                     <meta name="twitter:image" content={'https://www.seekfirst.ltd' + _.get(this.props, 'pageContext.frontmatter.content_img_path')} />
                     <meta name="twitter:creator" content="@goseekfirst" />
+                    <link rel="icon" href="https://www.seekfirst.ltd/images/seekfirst-logo.png"></link>
                     <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,700,700i" rel="stylesheet"/>
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
                     <script data-name="BMC-Widget" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="seekfirst" data-description="Support me on Buy me a coffee!" data-message="Thank you for visiting. You can now buy me a coffee!" data-color="#79D6B5" data-position="" data-x_margin="18" data-y_margin="18"></script>
