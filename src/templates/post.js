d0a4f3fc3fa6bc7527387e26309ae1346ef6adb6
joeyology <josemari.taleno@gmail.com>
2020-09-07 15:23:29 +0800

    Update post.js
    Added Author

--- src/templates/post.js
+++ src/templates/post.js
@@ -15,6 +15,11 @@ export default class Post extends React.Component {
                   <header className="post-header inner-small">
                     <h1 className="post-title line-top">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
                     <div className="post-meta">
+                      {_.get(this.props, 'pageContext.frontmatter.author') && 
+                      <p className="published">
+                        {_.get(this.props, 'pageContext.frontmatter.author')} />
+                      </p>
+                      }
                       <time className="published"
                         dateTime={moment(_.get(this.props, 'pageContext.frontmatter.date')).strftime('%Y-%m-%d %H:%M')}>{moment(_.get(this.props, 'pageContext.frontmatter.date')).strftime('%B %d, %Y')}</time>
                     </div>
